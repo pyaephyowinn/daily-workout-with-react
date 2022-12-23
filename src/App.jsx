392141863f0fa5a0d16517db4ca4c18015752eab
pyaephyowinn <pyaephyowinn@gmail.com>
2022-12-23 10:51:17 +0630

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css"
+import "./index.css"
+import DropDown from "./components/dropdown";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Checkout the branches for the features</h1>
+    <div className="bg-slate-700 text-slate-100 min-h-screen min-w-screen">
+      <h2 className="text-3xl text-center">hello world</h2>
+      <DropDown />
     </div>
   );
 }

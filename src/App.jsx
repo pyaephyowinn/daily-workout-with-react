d177b32199c518d132886513da0c3622dd92cbe5
pyaephyowinn <pyaephyowinn@gmail.com>
2022-12-26 23:59:20 +0630

    add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css"
-
 function App() {
   return (
-    <div className="container">
-      <h1>Checkout the branches for the features</h1>
+    <div className="min-h-screen min-w-screen bg-gray-700 text-gray-100 flex items-center justify-center">
+      <h1 className="text-5xl">Checkout the branches for the features</h1>
     </div>
   );
 }
